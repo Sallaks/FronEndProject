c3ff7c115426e3d72cbb80ab82f7cbcf2f0e4fc5
Manuel Gainza <manuel.gainza@mangoodev.com>
2022-09-13 19:44:27 -0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./Pages/login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./Pages/register/register.module').then(m => m.RegisterModule) }];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./public/pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./public/pages/register/register.module').then(m => m.RegisterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
