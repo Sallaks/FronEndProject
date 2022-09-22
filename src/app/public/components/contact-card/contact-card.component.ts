9a1ab6bdd39251f1c8d4f0e187eea78946441d6d
Manuel Gainza <manuel.gainza@mangoodev.com>
2022-09-22 18:46:34 -0300

    finish styles

--- src/app/public/components/contact-card/contact-card.component.ts
+++ src/app/public/components/contact-card/contact-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-card',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ContactCardComponent implements OnInit {
 
   constructor() { }
+
+  @Input() completeName!: string;
 
   ngOnInit(): void {
   }
