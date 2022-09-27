94d4d25f68fe5e2ccf45b3ecfe5144f37e93220e
Alvaro de la Peña <alvaro.ivan.dlp@gmail.com>
2022-09-27 16:54:37 -0300

    contactsInterface

---
+++ src/app/core/interfaces/contact.ts
@@ -0,0 +1,73 @@
+export interface Contact {
+    Name: string,
+    TelephoneNumber: string,
+    CelularNumber: string,
+    id: string,
+}
+
+export const defaultContact:Contact = {
+    Name: "First name",
+    CelularNumber: "+5412345678",
+    TelephoneNumber:"34128272",
+    id:""
+}
+
+console.log(defaultContact)
+
+export interface ContactJasonPlaceholder{
+
+}
+
+
+
+[{
+    "id": 1,
+    "first_name": "Gregoor",
+    "CelularNumber": "959-229-5000",
+    "TelephoneNumber": "946-793-7717"
+  }, {
+    "id": 2,
+    "first_name": "Janey",
+    "CelularNumber": "362-106-2026",
+    "TelephoneNumber": "495-370-2647"
+  }, {
+    "id": 3,
+    "first_name": "Torey",
+    "CelularNumber": "301-369-9194",
+    "TelephoneNumber": "673-372-9234"
+  }, {
+    "id": 4,
+    "first_name": "Estevan",
+    "CelularNumber": "546-320-7339",
+    "TelephoneNumber": "728-429-1417"
+  }, {
+    "id": 5,
+    "first_name": "Lavinie",
+    "CelularNumber": "991-161-5946",
+    "TelephoneNumber": "945-618-7996"
+  }, {
+    "id": 6,
+    "first_name": "Raye",
+    "CelularNumber": "219-885-7406",
+    "TelephoneNumber": "384-679-3317"
+  }, {
+    "id": 7,
+    "first_name": "Ollie",
+    "CelularNumber": "178-906-0545",
+    "TelephoneNumber": "433-151-0926"
+  }, {
+    "id": 8,
+    "first_name": "Garvey",
+    "CelularNumber": "846-349-8016",
+    "TelephoneNumber": "693-964-0417"
+  }, {
+    "id": 9,
+    "first_name": "Drud",
+    "CelularNumber": "995-312-5834",
+    "TelephoneNumber": "528-675-7834"
+  }, {
+    "id": 10,
+    "first_name": "Brooke",
+    "CelularNumber": "641-976-5995",
+    "TelephoneNumber": "328-571-9202"
+  }]
