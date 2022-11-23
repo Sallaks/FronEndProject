0a26c5f5f780256b639d6fcb940007fb9580118a
Manuel Gainza Alvarez <gainza2014@gmail.com>
2022-11-23 19:45:47 -0300

    route contact-details and fix styles

--- src/app/core/interfaces/contact.ts
+++ src/app/core/interfaces/contact.ts
@@ -1,11 +1,10 @@
-import { user } from "./user"
-
 export interface Contact {
   id: number,
   name: string,
   telephoneNumber: string,
   cellPhoneNumber: string,
   description: string,
-  user: user,
-  userId: number
 }
+
+//TODO: add user and userId
+//TODO: add interface to contact details
--- src/app/public/pages/contact-details/contact-details.component.ts
+++ src/app/public/pages/contact-details/contact-details.component.ts
@@ -10,21 +10,40 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ContactDetailsComponent implements OnInit {
 
-  contact: Contact | undefined
+  contact: Contact = {
+    id: 0,
+    name: "",
+    telephoneNumber: "",
+    cellPhoneNumber: "",
+    description: "",
+  }
   id: number | undefined
 
   constructor(private contactService: ContactService, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.params.subscribe(params => {
+      console.log(params)
       this.id = params['id'];
+      this.getContactDetails(this.id!);
     })
-    this.getContactDetails(this.id!)
   }
 
   getContactDetails(id: number) {
     this.contactService.getContactDetails(id).then(r => this.contact = r);
   }
+
+  getinitials(): string {
+    return this.contact.name
+    .slice(0, 1)
+    .concat(
+        this.contact.name.slice(
+            this.contact.name.length - 1,
+            this.contact.name.length
+        )
+    );
+  }
+
 
 }
